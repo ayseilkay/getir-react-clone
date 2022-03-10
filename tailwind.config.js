8dee1757860ac9aa84895b596da95dfbc235e149
ayseilkay <aayseilkay@gmail.com>
2022-03-11 00:30:07 +0300

    added HeroSection.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,20 +4,11 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      backgroundColor: {
+      colors:{
         'brand-color': "#4c3398",
         'primary-brand-color': "#5d3ebc",
-        'secondary-brand-color': "#7849f7"
-      },
-      gradientColorStops:{
-        'brand-color': "#4c3398",
-        'primary-brand-color': "#5d3ebc",
-        'secondary-brand-color': "#7849f7"
-      },
-      textColor:{
-        'brand-color': "#4c3398",
-        'primary-brand-color': "#5d3ebc",
-        'secondary-brand-color': "#7849f7"
+        'secondary-brand-color': "#7849f7",
+        'primary-button-color':"#ffd300"
       }
     },
   },
