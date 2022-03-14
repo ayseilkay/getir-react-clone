3af1b6d87bbb3aa25ae91649a9a2844088790662
ayseilkay <aayseilkay@gmail.com>
2022-03-15 00:26:24 +0300

    responsive design

--- src/components/ui/Product.js
+++ src/components/ui/Product.js
@@ -2,13 +2,13 @@ import React from 'react'
 import {FiPlus} from 'react-icons/fi'
 function Product({product:{id,title,image,alt,price}}) {
   return (
-    <div className='bg-white flex flex-col items-center text-center gap-y-1 p-3 relative'>
+    <div className='bg-white flex flex-col items-center text-center gap-y-1 p-3 relative w-full h-full'>
         <div className=''>
         <button className='absolute top-2 right-8 w-12 h-12 rounded-lg border border-gray-200 flex items-center justify-center text-primary-brand-color drop-shadow-md bg-white transition-colors  hover:border-brand-color'><FiPlus size={16}/></button>
         </div>
         <img src={image} alt={title}/>
         <div className='font-semibold text-sm text-brand-color'>{'₺'}{price}</div>
-        <div className='text-color-black font-semibold'>{title}</div>
+        <div className='text-color-black font-semibold flex items-center justify-center flex-shrink-0'>{title}</div>
         <div className='text-sm text-color-gray tracking-tight'>{alt}</div>
     </div>
   )
